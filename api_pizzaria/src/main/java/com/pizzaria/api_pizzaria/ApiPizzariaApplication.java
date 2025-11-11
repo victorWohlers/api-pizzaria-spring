d19ef84f2250b8223098e35807c15889b0d219f5
victor <victor.lima@cerc.com>
2025-11-11 20:59:44 -0300

    Initial commit do projeto Spring Boot

---
+++ api_pizzaria/src/main/java/com/pizzaria/api_pizzaria/ApiPizzariaApplication.java
@@ -0,0 +1,13 @@
+package com.pizzaria.api_pizzaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPizzariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPizzariaApplication.class, args);
+	}
+
+}
---
+++ api_pizzaria/src/test/java/com/pizzaria/api_pizzaria/ApiPizzariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizzaria.api_pizzaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPizzariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
